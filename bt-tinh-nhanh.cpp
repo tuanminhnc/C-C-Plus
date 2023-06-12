514f1c96d407e5f8f9c4139a290953c9e36d75d5
Nguyen Cong Tuan Minh <126317770+tuanminhnc@users.noreply.github.com>
2023-06-12 07:40:12 +0000

    Bai tap tinh nhanh

---
+++ bt-tinh-nhanh.cpp
@@ -0,0 +1,35 @@
+/*
+    Bai tap tinh nhanh
+Time 2:18 PM 
+6/12/2023
+*/
+#include <iostream>
+using namespace std;
+
+//Begin main function
+int main() {
+
+    /*
+        1. Thong bao ra man hinh bai tap tinh nhanh
+        2. Thuc hien tinh
+    */
+    
+    //Buoc 1
+    cout << "Bai tap tinh nhanh:" << endl;
+    cout << "a) 101^2     b) 199^2    c) 47.53" << endl << endl;
+    //Buoc 2
+    cout << "a) 101^2 = (100 + 1)^2" << endl;
+    cout << "= 100^2 + 2*100*1 + 1^2" << endl;
+    cout << "= 10000 + 200 + 1" << endl;
+    cout << "= 10201" << endl << endl;
+    cout << "b) 199^2 = (100 + 99)^2" << endl;
+    cout << "= 100^2 + 2*100*99 + 99^2" << endl;
+    cout << "= 10000 + 19800 + 9801" << endl;
+    cout << "= 39601" << endl << endl;
+    cout << "c) 47*53 = (50 - 3) * (50 + 3)" << endl;
+    cout << "= 50*(50 + 3) - 3*(50 + 3)" << endl;
+    cout << "= 2500 + 150 - 150 - 9" << endl;
+    cout << "= 2500 - 9" << endl;
+    cout << "= 2491" << endl;
+    return 0;
+}
